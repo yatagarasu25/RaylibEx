cb01697f14a244c0ffc0355b3e1b5402d7be0ebd
Igor Karatayev <igor.cwer@gmail.com>
2021-07-20 00:11:38 +0300

    adding simple keyframe animation support.

--- RaylibEx.Texture.cs
+++ RaylibEx.Texture.cs
@@ -9,7 +9,7 @@ namespace Raylib_cs
 
 		public static Texture2D GenTextureColors(int width, int height, colorb[] colors)
 		{
-			using var image = LockImage(GenImageColors(width, height, colors));
+			using var image = Lock(GenImageColors(width, height, colors));
 			return Raylib.LoadTextureFromImage(image._);
 		}
 	}
